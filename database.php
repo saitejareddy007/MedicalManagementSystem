c873ed78d9eca0f1489e5d139b5cb9814df5a075
saitejareddy007 <tejareddy.d321@gmail.com>
2018-05-21 23:12:30 +0530

    updated

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-	$con = mysqli_connect("localhost","root","sai","usersai");	
+	$con = mysqli_connect("localhost","root","sai","MedicalManagementSystem");	
 	if(mysqli_connect_errno()){
 		echo 'Failed to connect MySQL'.mysqli_connect_error();
 	}
